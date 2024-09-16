d97dedb480a9ff7bef3983e7ad30c56bb90bec0e
lucasbonafe1 <l2005bonafe@gmail.com>
2024-09-15 23:21:38 -0300

    fix: CheckedListBox funcionando corretamente

--- estudosFormsCsharp/F_CheckedListBox.cs
+++ estudosFormsCsharp/F_CheckedListBox.cs
@@ -16,5 +16,55 @@ namespace estudosFormsCsharp
         {
             InitializeComponent();
         }
+
+        private void btn_mostrarSelecionados_Click(object sender, EventArgs e)
+        {
+            string txt = "";
+
+            foreach (string t in clb_transportes.CheckedItems)
+            {
+                txt += t + ", ";
+            }
+
+            MessageBox.Show(txt);
+        }
+
+        private void btn_limparLista_Click(object sender, EventArgs e)
+        {
+            clb_transportes.Items.Clear();
+        }
+
+        private void btn_resetList_Click(object sender, EventArgs e)
+        {
+            clb_transportes.Items.Clear();
+
+            List<string> tr = new List<string>();
+
+            tr.Add("Carro");
+            tr.Add("Avião");
+            tr.Add("Navio");
+            tr.Add("Ônibus");
+
+            clb_transportes.Items.AddRange(tr.ToArray());
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (clb_transportes.Text != "")
+            {
+                clb_transportes.Items.Add(tb_novoTransporte.Text);
+                clb_transportes.Items.Clear();
+                clb_transportes.Focus();
+            }
+        }
+
+        private void tb_novoTransporte_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void F_CheckedListBox_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
